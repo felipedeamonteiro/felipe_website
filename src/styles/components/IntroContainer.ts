0a87e30584ba3e36e8259c3f7277c72a54cbcd6b
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-12-05 23:38:59 -0300

    Add next-images and its config so typescript can understand images formats

--- src/styles/components/IntroContainer.ts
+++ src/styles/components/IntroContainer.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
+import backgroundImage from '../../../public/background_image.jpg';
 
-import backgroundImage from '../../assets/background_image.jpg';
 
 export const Container = styled.div`
   background: url(${backgroundImage}) no-repeat center;
