19af3dea6c4a51165a1a55b43feafe4cde6d60b8
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-12-08 23:58:22 -0300

    Add image and description in AboutMe

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,10 +1,82 @@
 import React from 'react';
 
+import FotoFelipe from '../../public/Foto_felipe2.png';
 import { Container } from '../styles/components/AboutMe';
 
 const AboutMe: React.FC = () => (
   <Container>
-    <h1>about me</h1>
+    <div className="skills-wrapper">
+      <div className="about-me">
+        <img src={FotoFelipe} alt="Foto de Felipe"/>
+        <div className="label_bold">Sobre mim</div>
+        <div className="description">
+          Sou Engenheiro FullStack.
+          <br />
+          Tenho uma paixão por tecnologia e por construir!
+          <br />
+          Mas contruir o que?
+          <br />
+          Construir soluções inteligentes para problemas diversos.{' '}
+          <br />
+          <span className="page-link-highlight">
+            Quer saber mais? Entre em contato e vamos tomar um café.
+          </span>
+        </div>
+      </div>
+      
+      <div className="skills-bars">
+
+        <div className="bar-flex">
+          <div className="bar-fill">
+            <div className="tag bold flex">CSS</div>
+          </div>
+          <span>90%</span>
+        </div>
+
+        <div className="bar-flex">
+          <div className="bar-fill">
+            <div className="tag bold flex">HTML</div>
+          </div>
+          <span>90%</span>
+        </div>
+
+        <div className="bar-flex">
+          <div className="bar-fill">
+            <div className="tag bold flex">Javascript</div>
+          </div>
+          <span>70%</span>
+        </div>
+        
+        <div className="bar-flex">
+          <div className="bar-fill">
+            <div className="tag bold flex">React</div>
+          </div>
+          <span>70%</span>
+        </div>
+
+        <div className="bar-flex">
+          <div className="bar-fill">
+            <div className="tag bold flex">React-Native</div>
+          </div>
+          <span>65%</span>
+        </div>
+
+        <div className="bar-flex">
+          <div className="bar-fill">
+            <div className="tag bold flex">Nodejs</div>
+          </div>
+          <span>60%</span>
+        </div>
+
+        <div className="bar-flex">
+          <div className="bar-fill">
+            <div className="tag bold flex">Python</div>
+          </div>
+          <span>45%</span>
+        </div>
+
+      </div>
+    </div>
   </Container>
 );
 
--- src/styles/components/AboutMe.ts
+++ src/styles/components/AboutMe.ts
@@ -1,3 +1,29 @@
-import style from 'styled-components';
+import styled from 'styled-components';
 
-export const Container = style.div``;
+export const Container = styled.div`
+  height: 890px;
+  padding: 30px;
+
+  .skills-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .about-me{
+
+      img {
+        width: 250px;
+        height: 250px;
+        border-radius: 50%;
+        border: 4px solid #00CDE2;
+        object-fit: cover;
+      }
+    }
+
+    .skills-bars {
+
+    }
+  }
+
+  
+`;
