bfe4cc11a47b946b2a4253e9ab9a95bddc4275db
Felipe Monteiro <felipedeamonteiro@gmail.com>
2021-06-09 18:15:48 -0300

    Add intersectionObserver Method

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,128 +1,145 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import FotoFelipe from '../../public/Foto_felipe2.png';
 import LogoUnicamp from '../../public/unicamp.png';
 import { Container } from '../styles/components/AboutMe';
+import intersectionMethod from '../utils/intersectionMethod';
 
-const AboutMe: React.FC = () => (
-  <Container id="section-sobreMim">
-    <h1>Sobre mim</h1>
-    <div className="skills-wrapper">
-      <div className="about-me">
-        <img src={FotoFelipe} alt="Foto de Felipe"/>
-        <div className="label_bold">Quem sou eu?</div>
-        <div className="description">
-          Sou Engenheiro FullStack.
-          <br />
-          Tenho paixão por tecnologia e por construir!
-          <br />
-          <p>Mas construir o que?</p>
-          Construir soluções inteligentes para problemas diversos.{' '}
-          <br />
-          <span className="page-link-highlight">
-            Quer saber mais? Entre em contato e vamos tomar um café.
-          </span>
-        </div>
-      </div>
-      
-      <div className="skills-bars">
+const AboutMe: React.FC = () => {
+  const ref = useRef();
+  const inViewport = intersectionMethod(ref, '0px');
+  // useEffect(() => {
+  //   const aboutMeSection = document.getElementById('section-sobreMim');
+  //   window.addEventListener('scroll', () => {
+  //     if (aboutMeSection.scrollIntoView(true)) {
+  //       console.log(aboutMeSection);
+  //     }
+  //   });
+  // }, []);
+  if (inViewport) {
+    console.log('in viewport:', ref.current);
+}
 
-        <div className="bar-flex">
-          <div className="bar-content">
-            <div className="bar-tag">
-              CSS
-            </div>
-            <div className="bar-fill-css">
-              <span>90%</span>
-            </div>
-          </div>
-          
-        </div>
-
-        <div className="bar-flex">
-          <div className="bar-content">
-            <div className="bar-tag">HTML
-            </div>
-            <div className="bar-fill-html">
-              <span>90%</span>
-            </div>
-          </div>
-        </div>
-
-        <div className="bar-flex">
-          <div className="bar-content">
-            <div className="bar-tag">JavaScript
-            </div>
-            <div className="bar-fill-javascript">
-              <span>80%</span>
-            </div>
-          </div>
-        </div>
-
-        <div className="bar-flex">
-          <div className="bar-content">
-            <div className="bar-tag">React
-            </div>
-            <div className="bar-fill-react">
-              <span>80%</span>
-            </div>
+  return (
+    <Container ref={ref} id="section-sobreMim">
+      <h1>Sobre mim</h1>
+      <div className="skills-wrapper">
+        <div className="about-me">
+          <img src={FotoFelipe} alt="Foto de Felipe"/>
+          <div className="label_bold">Quem sou eu?</div>
+          <div className="description">
+            Sou Engenheiro FullStack.
+            <br />
+            Tenho paixão por tecnologia e por construir!
+            <br />
+            <p>Mas construir o que?</p>
+            Construir soluções inteligentes para problemas diversos.{' '}
+            <br />
+            <span className="page-link-highlight">
+              Quer saber mais? Entre em contato e vamos tomar um café.
+            </span>
           </div>
         </div>
         
-        <div className="bar-flex">
-          <div className="bar-content">
-            <div className="bar-tag">React-Native
+        <div className="skills-bars">
+
+          <div className="bar-flex">
+            <div className="bar-content">
+              <div className="bar-tag">
+                CSS
+              </div>
+              <div className="bar-fill-css">
+                <span>90%</span>
+              </div>
             </div>
-            <div className="bar-fill-react-native">
-              <span>75%</span>
+            
+          </div>
+
+          <div className="bar-flex">
+            <div className="bar-content">
+              <div className="bar-tag">HTML
+              </div>
+              <div className="bar-fill-html">
+                <span>90%</span>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="bar-flex">
-          <div className="bar-content">
-            <div className="bar-tag">Node.js
-            </div>
-            <div className="bar-fill-nodejs">
-              <span>70%</span>
-            </div>
-          </div>
-        </div>
-
-        <div className="bar-flex">
-          <div className="bar-content">
-            <div className="bar-tag">Python
-            </div>
-            <div className="bar-fill-python">
-              <span>60%</span>
+          <div className="bar-flex">
+            <div className="bar-content">
+              <div className="bar-tag">JavaScript
+              </div>
+              <div className="bar-fill-javascript">
+                <span>80%</span>
+              </div>
             </div>
           </div>
+
+          <div className="bar-flex">
+            <div className="bar-content">
+              <div className="bar-tag">React
+              </div>
+              <div className="bar-fill-react">
+                <span>80%</span>
+              </div>
+            </div>
+          </div>
+          
+          <div className="bar-flex">
+            <div className="bar-content">
+              <div className="bar-tag">React-Native
+              </div>
+              <div className="bar-fill-react-native">
+                <span>75%</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="bar-flex">
+            <div className="bar-content">
+              <div className="bar-tag">Node.js
+              </div>
+              <div className="bar-fill-nodejs">
+                <span>70%</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="bar-flex">
+            <div className="bar-content">
+              <div className="bar-tag">Python
+              </div>
+              <div className="bar-fill-python">
+                <span>60%</span>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div className="degree-div">
+        <div className="text-center">
+          <h2 className="section-heading text-uppercase">FORMAÇÃO ACADÊMICA</h2>
+
+          <h3 className="section-subheading text-muted">Desenvolvimento técnico constante em instituição de Ensino Superior de renome no Brasil</h3>
+        </div>
+        <div className="box1">
+          <img alt="Logo Unicamp" className="unicamp-img" src={LogoUnicamp} />
+          <h4 className="service">Graduação em Engenharia Mecânica - UNICAMP - Concluído em Janeiro de 2015</h4>
         </div>
 
-      </div>
-    </div>
-    <div className="degree-div">
-      <div className="text-center">
-        <h2 className="section-heading text-uppercase">FORMAÇÃO ACADÊMICA</h2>
+        <div className="box2">
+          <img alt="Logo Unicamp" className="unicamp-img" src={LogoUnicamp} />
+          <h4 className="service">Mestrado em Ciência da Computação - Inteligência Artificial e Robótica - UNICAMP - Interrompido</h4>
+        </div>
 
-        <h3 className="section-subheading text-muted">Desenvolvimento técnico constante em instituição de Ensino Superior de renome no Brasil</h3>
+        <div className="box3">
+          <img alt="Logo Unicamp" className="unicamp-img" src={LogoUnicamp} />
+          <h4 className="service">Graduação em Engenharia Elétrica - Focado em desenvlvimento de software e Inteligência Artificial - UNICAMP - Em curso desde 2021</h4>
+        </div>
       </div>
-      <div className="box1">
-        <img alt="Logo Unicamp" className="unicamp-img" src={LogoUnicamp} />
-        <h4 className="service">Graduação em Engenharia Mecânica - UNICAMP - Concluído em Janeiro de 2015</h4>
-      </div>
-
-      <div className="box2">
-        <img alt="Logo Unicamp" className="unicamp-img" src={LogoUnicamp} />
-        <h4 className="service">Mestrado em Ciência da Computação - Inteligência Artificial e Robótica - UNICAMP - Interrompido</h4>
-      </div>
-
-      <div className="box3">
-        <img alt="Logo Unicamp" className="unicamp-img" src={LogoUnicamp} />
-        <h4 className="service">Graduação em Engenharia Elétrica - Focado em desenvlvimento de software e Inteligência Artificial - UNICAMP - Em curso desde 2021</h4>
-      </div>
-    </div>
-  </Container>
-);
+    </Container>
+  )
+};
 
 export default AboutMe;
